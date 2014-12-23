1717626747548b0468780618595a6f694ae55252
DH3ALEJANDRO <dhe.my.x100pre@gmail.com>
2014-12-22 21:30:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window, MainViewManager*/
+/*global define, $, brackets */
 define(function (require, exports, module) {
     "use strict";
-    var CommandManager        = brackets.getModule("command/CommandManager"),
-        Dialogs               = brackets.getModule("widgets/Dialogs"),
-        DefaultDialogs        = brackets.getModule("widgets/DefaultDialogs"),
-        ExtensionUtils        = brackets.getModule("utils/ExtensionUtils"),
-        Menus                 = brackets.getModule("command/Menus"),
-        ProjectManager        = brackets.getModule("project/ProjectManager"),
-        NativeApp             = brackets.getModule('utils/NativeApp'),
-        nameContextMenu       = "Open In Browser",
-        hw                    = nameContextMenu,
-        cmd_auted_id          = "hello.auto",
-        filesMenu             = Menus.getMenu(Menus.AppMenuBar.FILE_MENU),
-        contextMenu           = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU);
-    function OPINBR() {
-        var contentBra = MainViewManager.getCurrentlyViewedFile()._path;
-        var titleModal = ProjectManager.makeProjectRelativeIfPossible(contentBra);
-        var booly = new RegExp(".htm").test(titleModal);
-        if (booly) {
-            var url = 'file:///';
-            NativeApp.openURLInDefaultBrowser(url + titleModal);
+    var CommandManager = brackets.getModule("command/CommandManager"),
+        Menus          = brackets.getModule("command/Menus"),
+        Dialogs        = brackets.getModule("widgets/Dialogs"),
+        ProjectManager = brackets.getModule("project/ProjectManager"),
+        AppInit        = brackets.getModule('utils/AppInit'),
+        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        NativeApp      = brackets.getModule('utils/NativeApp'),
+        contextMenu    = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU),
+        menu           = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU),
+        titleRegister  = "Open in Browser",
+        shortCut       = "Ctrl-Shift-Alt-R",
+        COMMAND_ID     = "rfghdfhbdfydfg.open";
+    function startOp() {
+        var entry = ProjectManager.getSelectedItem().fullPath;
+        var booly = ProjectManager.getSelectedItem()._name;
+        if (/\.(htm|html|py|php|svg|jpg|jpeg|gif|png|ico|asp)$/i.test(booly)) {
+            NativeApp.openURLInDefaultBrowser('file:///' + entry);
         } else {
-            var text = "is not a valid page to display" + '<br>' + titleModal;
-            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO, hw, text);
+            var text = "is not a valid page to display" + '<br>' + entry;
+            Dialogs.showModalDialog(COMMAND_ID, booly, text);
         }
     }
-    CommandManager.register(nameContextMenu, cmd_auted_id, $.proxy(OPINBR));
-    contextMenu.addMenuItem(cmd_auted_id, "Ctrl-Shift-Y");
-    filesMenu.addMenuDivider('before', 'hello.worl.min');
-    filesMenu.addMenuItem(cmd_auted_id, "Ctrl-Shift-Y");
-    //filesMenu.removeMenuDivider();
+    CommandManager.register(titleRegister, COMMAND_ID, startOp);
+    contextMenu.addMenuItem(COMMAND_ID, shortCut);
+    menu.addMenuItem(COMMAND_ID, shortCut);
+    function addButton() {
+        var $toolbar = $('#main-toolbar .buttons');
+        var _button = $('<a/>').attr({
+            'id': 'open-notification',
+            'title': titleRegister
+        }).click(startOp).appendTo($toolbar);
+    }
+    AppInit.appReady(function () {
+        ExtensionUtils.loadStyleSheet(module, "toolbar.css");
+        addButton();
+    });
 });
