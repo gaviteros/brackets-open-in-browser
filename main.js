acddee447b20cc16289b26f50ee736d1c2b1895a
DH3ALEJANDRO <dhe.my.x100pre@gmail.com>
2015-05-13 09:16:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,62 +2,69 @@
 /*global define, $, brackets */
 define(function (require, exports, module) {
     "use strict";
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus"),
-        Dialogs        = brackets.getModule("widgets/Dialogs"),
-        ProjectManager = brackets.getModule("project/ProjectManager"),
-        AppInit        = brackets.getModule('utils/AppInit'),
+    var ProjectManager = brackets.getModule("project/ProjectManager"),
+        AppInit = brackets.getModule('utils/AppInit'),
         ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        NativeApp      = brackets.getModule('utils/NativeApp'),
+        NativeApp = brackets.getModule('utils/NativeApp'),
+        MainViewManager = brackets.getModule("view/MainViewManager"),
         obtainLanguaje = brackets.getLocale(),
-        contextMenu    = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU),
-        menu           = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU),
-        shortCut       = "Ctrl-Shift-Q",
-        COMMAND_ID     = "rfghdfhbdfydfg.open",
         titleRegister,
+        nothing,
         popupTaxt;
+
     function returnLanguaje() {
         if (/it/gi.test(obtainLanguaje)) {
             popupTaxt = "Questa non è una pagina valida per visualizzazione";
             titleRegister = "Mostra nel Browser";
+            nothing = "nulla qui intorno";
         } else if (/es/gi.test(obtainLanguaje)) {
             popupTaxt = "Esta no es una página válida para visualizar";
             titleRegister = "Mostrar en el Navegador";
+            nothing = "nada por aqui";
         } else if (/fr/gi.test(obtainLanguaje)) {
             popupTaxt = "Page à afficher invalide";
             titleRegister = "Ouvrir dans le Navigateur";
+            nothing = "rien ici";
         } else if (/pt/gi.test(obtainLanguaje)) {
             popupTaxt = "Não é um arquivo válido para exibir";
             titleRegister = "Abrir no Navegador";
+            nothing = "nada por aqui";
         } else {
             popupTaxt = "is not a valid page to display";
             titleRegister = "Open in Browser";
+            nothing = "nothing around here";
         }
-        return [popupTaxt, titleRegister];
+        return [popupTaxt, titleRegister, nothing];
     }
     returnLanguaje();
-    function startOp() {
-        var entry = ProjectManager.getSelectedItem().fullPath;
-        var booly = ProjectManager.getSelectedItem()._name;
-        if (/\.(htm|html|py|php|svg|jpg|jpeg|gif|png|ico|asp)$/i.test(booly)) {
-            NativeApp.openURLInDefaultBrowser('file:///' + entry);
-        } else {
-            var text = popupTaxt + '<br>' + entry;
-            Dialogs.showModalDialog(COMMAND_ID, booly, text);
+
+    function removeButton(el) {
+        if ($(el).length) {
+            $(el).remove();
         }
     }
-    CommandManager.register(titleRegister, COMMAND_ID, startOp);
-    contextMenu.addMenuItem(COMMAND_ID, shortCut);
-    menu.addMenuItem(COMMAND_ID);
-    function addButton() {
-        var $toolbar = $('#main-toolbar .buttons');
-        var _button = $('<a/>').attr({
-            'id': 'open-notification',
-            'title': titleRegister
-        }).click(startOp).appendTo($toolbar);
+
+    function startOp() {
+        MainViewManager.on("currentFileChange", function () {
+            var entry = ProjectManager.getSelectedItem().fullPath,
+                booly = ProjectManager.getSelectedItem()._name;
+            removeButton("#open-notification");
+            if (/\.(htm|html|py|php|svg|jpg|jpeg|gif|png|ico|asp)$/i.test(booly)) {
+                $('<a/>').attr({
+                    'id': "open-notification",
+                    'class': "openInBrowser",
+                    'title': titleRegister
+                }).click(function () {
+                    NativeApp.openURLInDefaultBrowser('file:///' + entry);
+                }).appendTo($('#main-toolbar .buttons'));
+            } else {
+                removeButton("#open-notification");
+            }
+        });
     }
+
     AppInit.appReady(function () {
         ExtensionUtils.loadStyleSheet(module, "toolbar.css");
-        addButton();
+        startOp();
     });
 });
